3725279d0fb40f669fc927c9e5fbbb4e794e7a45
Vardan Akopian <vakopian+github@gmail.com>
2014-05-03 21:24:23 -0700

    support osx/homebrew

--- tools/make_package.sh
+++ tools/make_package.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-cd $(dirname $(readlink -f $0))/../source
+which greadlink >/dev/null || alias greadlink='readlink'
+self="$(greadlink -f $0 2>/dev/null || echo $(pwd -P)/$0)"
+source_dir=$(dirname "$self")/../source
+cd "$source_dir"
 
 echo "Creating a zip archive.."
 #Format date
 date=$(date +%Y-%m-%d)
 #Package variables
-version=$(grep "strVersion" install.php | grep -oE '[0-9].[0-9]')
-platform=$(grep "strPlatformVersion" install.php | grep -oE '[0-9].[0-9]')
+version=$(grep "strVersion" install.php | grep -oE '[0-9](.[0-9])+')
+platform=$(grep "strPlatformVersion" install.php | grep -oE '[0-9](.[0-9])+')
 package=$(grep "strName" install.php | grep -oE '\"[A-Za-z0-9]*\"' | tr -d '"')
 
 filename=${package}_${version}_${platform}_${date}.zip
